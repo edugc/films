100d3fe442fc1dd840655cd22e9f794d2cd2b86b
eduardo garcia <eduardo.garcia@coches.com>
2017-04-19 16:04:12 +0200

    creado mi primer migrate y ejecutado en sqlite

--- migrations/m170419_135136_movie.php
+++ migrations/m170419_135136_movie.php
@@ -1,19 +1,23 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m170419_135136_movie extends Migration
 {
     public function up()
     {
-
+        $this->createTable('movie',[
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT,
+        ]);
     }
 
     public function down()
     {
-        echo "m170419_135136_movie cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('movie');
+        return true;
     }
 
     /*
